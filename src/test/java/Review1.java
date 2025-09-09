7aa3d3c5add32e249002d03e15371b2a8657a74c
kumail-naqvi-ics4u1-01-25 <kumail.naqvi@student.tdsb.on.ca>
2025-09-09 17:13:30 +0000

    Kumail Review 1

---
+++ src/test/java/Review1.java
@@ -0,0 +1,29 @@
+public class Review1 {
+
+	public static void q1() {
+		System.out.println("There once was a man from St. Ives.");
+	}
+
+	public static void q2() {
+		int num1 = 9;
+        int num2 = 5;
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+	}
+
+	public static void q3() {
+		System.out.println("Hello\nHello again");
+	}
+
+	public static void q4() {
+		boolean boolean1 = true;
+        System.out.println(boolean1);
+	}
+
+	public static void q5() {
+		String String1 = "Hello";
+        String String2 = "World";
+        System.out.println(String1 +" "+ String2);
+	}
+
+}
